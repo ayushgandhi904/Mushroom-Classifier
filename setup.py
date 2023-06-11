57a76c3fa6f07a2986b7cf69a5c176020b02f606
Your Name <ayushgandhi904@gmail.com>
2023-06-11 20:59:42 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+from typing import List
+
+hyphen_e_dot = "-e ."
+
+def get_requirements(file_path:str)->List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        
+        if hyphen_e_dot in requirements:
+            requirements.remove(hyphen_e_dot)
+            
+    return requirements
+
+
+setup(
+    name = "Mushroom_Classifier",
+    version = "0.1",
+    author = "Ayush Gandhi",
+    author_email = "ayushgandhi904@gmail.com",
+    install_requires = get_requirements("requirements.txt"),
+    packages = find_packages()
+)
