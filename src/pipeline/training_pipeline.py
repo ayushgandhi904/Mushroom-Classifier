1b518a772f81e350ec02859127790ee78b412a42
Your Name <ayushgandhi904@gmail.com>
2023-06-19 12:12:59 +0530

    changes

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -19,6 +19,10 @@ if __name__ == "__main__":
     data_transformation = DataTransformation()
     train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data_path, test_data_path)
     
-    model_trainer = ModelTrainer()
-    model_trainer.initiate_model_training(train_arr, test_arr)
+    model_trainer=ModelTrainer()
+    model_trainer.initate_model_training(train_arr,test_arr)
+
+    
+    
+
     
